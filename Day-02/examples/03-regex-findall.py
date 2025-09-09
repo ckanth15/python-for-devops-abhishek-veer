04223e9bb7934be3afe89e23aaf5a06393555b3c
ckanth15 <ckanthus@gmail.com>
2025-09-09 14:53:12 -0700

    Update regex findall

--- Day-02/examples/03-regex-findall.py
+++ Day-02/examples/03-regex-findall.py
@@ -1,10 +1,23 @@
+# Method1 using re-search twice
 import re
 
+print("Using Method 1 ")
 text = "The quick brown fox"
-pattern = r"brown"
 
-search = re.search(pattern, text)
-if search:
-    print("Pattern found:", search.group())
+if re.search(r"quick", text) and re.search(r"brown", text):
+    print("Both patterns found")
 else:
-    print("Pattern not found")
+    print("One or both patterns not found")
+
+# Method 2: Using re.findall and sets
+# import re
+
+print("Using Method 2 ")
+text = "The quick brown fox"
+words = ["quick", "brown"]
+
+matches = re.findall(r"\b(?:quick|brown)\b", text)
+if set(words).issubset(matches):
+    print("Both patterns found")
+else:
+    print("One or both patterns not found")
